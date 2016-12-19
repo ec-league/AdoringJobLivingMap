71bf2a54839575078a8621ea745e13061e8a9eda
baiyp <baiyp@Ctrip.com>
2016-12-19 17:00:52 +0800

    Fix 406 error

--- src/main/java/com/ecleague/adoringjoblivingmap/common/util/JsonDto.java
+++ src/main/java/com/ecleague/adoringjoblivingmap/common/util/JsonDto.java
@@ -4,33 +4,58 @@ package com.ecleague.adoringjoblivingmap.common.util;
  * Created by yun.li on 2016/12/19.
  */
 public class JsonDto {
-    private int retCode;
-    private String retMessage;
-    private Object result;
+   private int retCode;
+   private String retMessage;
 
-    public JsonDto(Object result){
-        this.retCode = JsonDtoFlag.SUCCESS_CODE;
-        if(result != null){
-            this.result = result;
-        }
-        this.retMessage = JsonDtoFlag.SUCCESS_MESSAGE;
-    }
+   public int getRetCode() {
+      return retCode;
+   }
 
-    public JsonDto formSuccessJsonDto(){
-        this.retCode = JsonDtoFlag.SUCCESS_CODE;
-        this.retMessage = JsonDtoFlag.SUCCESS_MESSAGE;
-        return this;
-    }
+   public void setRetCode(int retCode) {
+      this.retCode = retCode;
+   }
 
-    public JsonDto formFailureJsonDto(Exception ex){
-        this.retCode = JsonDtoFlag.FAILURE_CODE;
-        this.retMessage = ex.getMessage();
-        return this;
-    }
+   public String getRetMessage() {
+      return retMessage;
+   }
 
-    public JsonDto(int returnCode,String returnMessage,Object result){
-        this.retCode = returnCode;
-        this.retMessage = returnMessage;
-        this.result = result;
-    }
+   public void setRetMessage(String retMessage) {
+      this.retMessage = retMessage;
+   }
+
+   private Object result;
+
+   public Object getResult() {
+      return result;
+   }
+
+   public void setResult(Object result) {
+      this.result = result;
+   }
+
+   public JsonDto(Object result) {
+      this.retCode = JsonDtoFlag.SUCCESS_CODE;
+      if (result != null) {
+         this.result = result;
+      }
+      this.retMessage = JsonDtoFlag.SUCCESS_MESSAGE;
+   }
+
+   public JsonDto formSuccessJsonDto() {
+      this.retCode = JsonDtoFlag.SUCCESS_CODE;
+      this.retMessage = JsonDtoFlag.SUCCESS_MESSAGE;
+      return this;
+   }
+
+   public JsonDto formFailureJsonDto(Exception ex) {
+      this.retCode = JsonDtoFlag.FAILURE_CODE;
+      this.retMessage = ex.getMessage();
+      return this;
+   }
+
+   public JsonDto(int returnCode, String returnMessage, Object result) {
+      this.retCode = returnCode;
+      this.retMessage = returnMessage;
+      this.result = result;
+   }
 }
